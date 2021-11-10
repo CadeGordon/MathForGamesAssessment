1feda3aa31da05e3e4238b000f38170a749ab48e
CadeGordon <88592762+CadeGordon@users.noreply.github.com>
2021-11-10 08:56:21 -0600

    Initialize Raylib

---
+++ MathForGames/Actor.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    
+    class Actor
+    {
+        private string _name;
+        private bool _started;
+        private Vector2 _forward = new Vector2(1,0);
+        private Collider _collider;
+        private Matrix4 _globalTransform = Matrix4.Identity;
+        private Matrix4 _localTransform = Matrix4.Identity;
+        private Matrix4 _translation = Matrix4.Identity;
+        private Matrix4 _rotation = Matrix4.Identity;
+        private Matrix4 _scale = Matrix4.Identity;
+        private Actor[] _children = new Actor[0];
+        private Actor _parent;
+        private Sprite _sprite;
+
+        /// <summary>
+        /// True if the start function has been called for this actor
+        /// </summary>
+        public bool Started
+        {
+            get { return _started; }
+        }
+
+        public Vector2 LocalPosition
+        {
+            get { return new Vector2(_translation.M02, _translation.M12); }
+            set 
+            {
+                SetTranslation(value.X, value.Y);
+            }
+        }
+
+        public void UpdateTransform()
+        {
+            if (Parent != null)
+                GlobalTransform = _parent.GlobalTransform * LocalTransform;
+            else
+                GlobalTransform = LocalTransform;
+
+            
+        }
+
+        public void AddChild(Actor child)
+        {
+            
+            Actor[] tempArray = new Actor[_children.Length + 1];
+
+           
+            for (int i = 0; i < _children.Length; i++)
+            {
+                tempArray[i] = _children[i];
+            }
+
+            
+            tempArray[_children.Length] = child;
+
+            child.Parent = this;
+
+            _children = tempArray;
+        }
+
+        public bool RemoveChild(Actor child)
+        {
+            //Create a variable to store if the removal was successful
+            bool childRemoved = false;
+
+            //Create a new array that is smaller than the original
+            Actor[] tempArray = new Actor[_children.Length - 1];
+
+            //Copy all values except the actor we don't want into the new array
+            int j = 0;
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                //If the actor that the loop is on is not the one to remove...
+                if (_children[i] != child)
+                {
+                    //...add the actor back into the new array
+                    tempArray[j] = _children[i];
+                    j++;
+                }
+                //Otherwise if this actor is the one to remove...
+                else
+                    //...set actorRemoved to true
+                    childRemoved = true;
+            }
+
+            //If the actor removal was successful...
+            if (childRemoved)
+                //...set the old array to be the new array
+                _children = tempArray;
+
+            return childRemoved;
+        }
+
+        public Vector2 WorldPosition
+        {
+            get { return new Vector2(_globalTransform.M02, _globalTransform.M12); }
+            set
+            {
+                SetTranslation(value.Y, value.X);
+            }
+        }
+
+        public Matrix4 GlobalTransform
+        {
+            get { return _globalTransform; }
+            set { _globalTransform = value; }
+        }
+
+        public Matrix4 LocalTransform
+        {
+            get { return _localTransform; }
+            set { _localTransform = value; }
+        }
+
+        public Actor Parent
+        {
+            get { return _parent; }
+            set { _parent = value; }
+        }
+
+        public Actor[] Children
+        {
+            get { return _children; }
+        }
+
+
+        public Vector2 Size
+        {
+            get { return new Vector2(_scale.M00, _scale.M11); }
+            set { SetScale(value.X, value.Y); }
+        }
+
+        
+
+        public Actor( float x, float y, string name = "Actor", string path = "" ) :
+            this( new Vector2 { X = x, Y = y }, name, path)
+        { }
+        
+        
+        
+        
+
+
+        public Actor( Vector2 position, string name = "Actor" , string path = "")
+
+        {
+            LocalPosition = position;
+            _name = name;
+
+            if (path != "")
+                _sprite = new Sprite(path);
+        }
+
+        public Vector2 Forward
+        {
+            get { return new Vector2(_rotation.M00, _rotation.M10); }
+            set 
+            {
+                Vector2 point = value.Normalized + LocalPosition;
+                LookAt(point);
+                    
+            }
+        }
+
+        public Sprite Sprite
+        {
+            get { return _sprite; }
+            set { _sprite = value; }
+        }
+
+        public Collider Collider
+        {
+            get { return _collider; }
+            set { _collider = value; }
+        }
+
+        public virtual void Start()
+        {
+            _started = true;
+        }
+
+        public virtual void Update(float deltaTime, Scene currentScene)
+        {
+            Rotate(2 * deltaTime);
+
+            _localTransform = _translation * _rotation * _scale;
+            Console.WriteLine(_name + ": " + LocalPosition.X + ", " + LocalPosition.Y);
+            UpdateTransform();
+
+        }
+
+        public virtual void Draw()
+        {
+            if (_sprite != null)
+                _sprite.Draw(GlobalTransform);
+        }
+
+        public void End()
+        {
+
+        }
+
+        public virtual void OnCollision(Actor actor, Scene currentScene)
+        {
+
+        }
+
+        /// <summary>
+        /// Check if this actor collided with another actor
+        /// </summary>
+        /// <param name="other">Thea ctor to check for a collision</param>
+        /// <returns>True if the distance bewteen  the actors is less than the radii of the two combined</returns>
+        public virtual bool CheckForCollision(Actor other)
+        {
+            //Retunr false if either actor doesnt have a collider attached
+            if (Collider == null || other.Collider == null)
+                return false;
+
+            return Collider.CheckCollision(other);
+        }
+
+        /// <summary>
+        /// Sets the position of the actor
+        /// </summary>
+        /// <param name="translationX">the new x position</param>
+        /// <param name="translationY">The new y position</param>
+        public void SetTranslation(float translationX, float translationY)
+        {
+            _translation = Matrix4.CreateTranslation(translationX, translationY);
+        }
+
+        /// <summary>
+        /// Changes the position of the actor by the given values
+        /// </summary>
+        /// <param name="translationX">the amount to move on x</param>
+        /// <param name="translationY">the amount to move on y</param>
+        public void Translate(float translationX, float translationY)
+        {
+            _translation *= Matrix4.CreateTranslation(translationX, translationY);
+        }
+
+        /// <summary>
+        /// Set the rotation of the actor
+        /// </summary>
+        /// <param name="radians">the angle of the new rotation in radians</param>
+        public void SetRotation(float radians)
+        {
+            _rotation = Matrix4.CreateRotation(radians);
+        }
+
+        /// <summary>
+        /// Adds a rotation to the current transformation rotations
+        /// </summary>
+        /// <param name="radians">the angle in radians to tunr</param>
+        public void Rotate(float radians)
+        {
+            _rotation *= Matrix4.CreateRotation(radians);
+        }
+
+        /// <summary>
+        /// Sets the scale of the actor
+        /// </summary>
+        /// <param name="x">The value to scale on the x axis</param>
+        /// <param name="y">The value to scale on the x axis</param>
+        public void SetScale(float x, float y)
+        {
+            _scale = Matrix4.CreateScale(x, y);
+            
+        }
+
+        /// <summary>
+        /// Scales the actor by the given amount
+        /// </summary>
+        /// <param name="x">The value to scale on the x axis</param>
+        /// <param name="y">The value to scale on the y axis</param>
+        public void Scale(float x, float y)
+        {
+            _scale *= Matrix4.CreateScale(x, y);
+        }
+
+        /// <summary>
+        /// Rotates the actor to face the given position
+        /// </summary>
+        /// <param name="position">The posittion the actor should be looking towards</param>
+        public void LookAt(Vector2 position)
+        {
+            //FInd the direction that the actor should look in
+            Vector2 direction = (position - LocalPosition).Normalized;
+
+            //Use the dot product to find the angle the actor needs to rotate
+            float dotProd = Vector2.DotProdcut(direction, Forward);
+
+            if (dotProd > 1)
+                dotProd = 1;
+
+            float angle = (float)Math.Acos(dotProd);
+
+            //Find a perpendicular vector to the direction 
+            Vector2 perpDirection = new Vector2(direction.Y, -direction.X);
+
+            //Find the dot prodcut of the perpendicluar vectoar and the current foward
+            float perpDot = Vector2.DotProdcut(perpDirection, Forward);
+
+            //If the result isnt 0, use it to change the sign of the angle to be either positive or negative
+            if (perpDot != 0)
+                angle *= -perpDot / Math.Abs(perpDot);
+
+            Rotate(angle);
+
+        }
+    }
+}
---
+++ MathLibrary/Matrix3.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLibrary
+{
+    public struct Matrix4
+    {
+        public float M00, M01, M02, M10, M11, M12, M20, M21, M22;
+
+        public Matrix4(float m00, float m01, float m02,
+                       float m10, float m11, float m12,
+                       float m20, float m21, float m22)
+        {
+            M00 = m00; M01 = m01; M02 = m02;
+            M10 = m10; M11 = m11; M12 = m12;
+            M20 = m20; M21 = m21; M22 = m22;
+        }
+
+        public static Matrix4 Identity
+        {
+            get
+            {
+                return new Matrix4(1, 0, 0,
+                                   0, 1, 0,
+                                   0, 0, 1);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new matric that has been rotated by the given value in radians
+        /// </summary>
+        /// <param name="raidans"></param>
+        /// <returns>The result of the rotation</returns>
+        public static Matrix4 CreateRotation(float radians)
+        {
+            return new Matrix4((float)Math.Cos(radians), (float)Math.Sin(radians), 0,
+                                -(float)Math.Sin(radians), (float)Math.Cos(radians), 0,
+                                0, 0, 1);
+
+        }
+
+        /// <summary>
+        /// Creates a new matix that has been translted by the
+        /// </summary>
+        /// <param name="x">The x position of the new matrix</param>
+        /// <param name="y">The y position of the new matrix </param>
+        /// <returns></returns>
+        public static Matrix4 CreateTranslation(float x, float y)
+        {
+            return new Matrix4(1, 0, x,
+                               0, 1, y,
+                               0, 0, 1);
+        }
+
+        /// <summary>
+        /// Creates a new matrix that has been scaled by the given value
+        /// </summary>
+        /// <param name="x">the value to use to scale the materix in the x axis</param>
+        /// <param name="y">the value to use to scale the materix in the y axis</param>
+        /// <returns>The result of the scale</returns>
+        public static Matrix4 CreateScale(float x, float y)
+        {
+            return new Matrix4(x, 0, 0,
+                               0, y, 0,
+                               0, 0, 1);
+        }
+        
+        public static Matrix4 operator +(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4  (lhs.M00 + rhs.M00, lhs.M01 + rhs.M01, lhs.M02 + rhs.M02,
+                                 lhs.M10 + rhs.M10, lhs.M11 + rhs.M11, lhs.M12 + rhs.M12,
+                                 lhs.M20 + rhs.M20, lhs.M21 + rhs.M21, lhs.M22 + rhs.M22);
+             
+        }
+
+        public static Matrix4 operator -(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4  (lhs.M00 - rhs.M00, lhs.M01 - rhs.M01, lhs.M02 - rhs.M02,
+                                 lhs.M10 - rhs.M10, lhs.M11 - rhs.M11, lhs.M12 - rhs.M12,
+                                 lhs.M20 - rhs.M20, lhs.M21 - rhs.M21, lhs.M22 - rhs.M22);
+        }
+
+        public static Matrix4 operator *(Matrix4 lhs, Matrix4 rhs)
+        {
+            return new Matrix4
+                (
+                  //Row 1, Column1
+                  lhs.M00 * rhs.M00 + lhs.M01 * rhs.M10 + lhs.M02 * rhs.M20,
+                  //Row 1, Column 2
+                  lhs.M00 * rhs.M01 + lhs.M01 * rhs.M11 + lhs.M02 * rhs.M21,
+                  //Row 1, Column 3
+                  lhs.M00 * rhs.M02 + lhs.M01 * rhs.M12 + lhs.M02 * rhs.M22,
+
+                  //Row 2, Column 1
+                  lhs.M10 * rhs.M00 + lhs.M11 * rhs.M10 + lhs.M12 * rhs.M20,
+                  //Row 2, Column 2
+                  lhs.M10 * rhs.M01 + lhs.M11 * rhs.M11 + lhs.M12 * rhs.M21,
+                  //Row 2, Column 3
+                  lhs.M10 * rhs.M02 + lhs.M11 * rhs.M12 + lhs.M12 * rhs.M22,
+
+                  //Row 3, Column 1
+                  lhs.M20 * rhs.M00 + lhs.M21 * rhs.M10 + lhs.M22 * rhs.M20,
+                  //Row 3 , Column 2
+                  lhs.M20 * rhs.M01 + lhs.M21 * rhs.M11 + lhs.M22 * rhs.M21,
+                  //Row 3, Coloum 2
+                  lhs.M20 * rhs.M02 + lhs.M21 * rhs.M12 + lhs.M22 * rhs.M22
+                );
+
+        }
+    }
+}
